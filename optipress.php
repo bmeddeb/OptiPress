805f867a2f670840045701bbcde3693645eddda7
Ben Meddeb <bmeddeb@asu.edu>
2025-10-01 14:59:14 -0700

    Release version 0.6.2
    - Updated version to 0.6.2 in all files
    - Created distribution package
    - Ready for release

--- optipress.php
+++ optipress.php
@@ -3,7 +3,7 @@
  * Plugin Name: OptiPress
  * Plugin URI: https://optipress.meddeb.me
  * Description: Image optimization and safe SVG handling for WordPress. Converts images to WebP/AVIF and enables secure SVG uploads.
- * Version:     0.5.9
+ * Version:     0.6.2
  * Requires at least: 6.7
  * Requires PHP: 7.4
  * Author: Ben Meddeb
@@ -17,7 +17,7 @@
 defined( 'ABSPATH' ) || exit;
 
 // Plugin constants
-define( 'OPTIPRESS_VERSION', '0.5.9' );
+define( 'OPTIPRESS_VERSION', '0.6.2' );
 define( 'OPTIPRESS_PLUGIN_FILE', __FILE__ );
 define( 'OPTIPRESS_PLUGIN_DIR', plugin_dir_path( __FILE__ ) );
 define( 'OPTIPRESS_PLUGIN_URL', plugin_dir_url( __FILE__ ) );
